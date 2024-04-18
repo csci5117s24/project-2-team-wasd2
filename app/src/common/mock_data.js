505d373a79b54ab609ad7144b6527e3ede855e93
Dominic Deiman <dominic.deiman@outlook.com>
2024-04-17 21:10:25 -0500

    adding some pictures for mock data; delete them later

--- app/src/common/mock_data.js
+++ app/src/common/mock_data.js
@@ -21,19 +21,19 @@ export var weightLogList = [
     {
         "value": 100,
         "unit": "kg",
-        "picture": "/cactus.svg",
+        "picture": "/delete_these_pictures_later/Heavy_weight.png",
         "timestamp": 1713202337616
     },
     {
         "value": 95,
         "unit": "kg",
-        "picture": "/cactus.svg",
+        "picture": "/delete_these_pictures_later/Middle_weight.png",
         "timestamp": 1713301337616
     },
     {
         "value": 92,
         "unit": "kg",
-        "picture": "/cactus.svg",
+        "picture": "/delete_these_pictures_later/Light_weight.png",
         "timestamp": 1713400348865
     }
 ];
