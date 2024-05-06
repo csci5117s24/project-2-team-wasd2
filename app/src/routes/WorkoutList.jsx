40c41756b5bc5eea5039ccd46ef218278ed428a7
wangling <wangling@wanglingdeMacBook-Pro.local>
2024-05-05 22:07:41 -0500

    fix: minor error

--- app/src/routes/WorkoutList.jsx
+++ app/src/routes/WorkoutList.jsx
@@ -56,7 +56,7 @@ function WorkoutList({  fetchTrigger }) {
         const updatedWorkout = {
             title: editFormData.title,
             description: editFormData.description,
-            calories: editFormData.calories
+            calories: parseInt(editFormData.calories)
         };
     
         try {
